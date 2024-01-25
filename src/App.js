29a9bd5166eb8c603cf94a5782df22d14be39f02
StyleIQCo <95829027+StyleIQCo@users.noreply.github.com>
2024-01-24 23:10:23 -0800

    ActionCard Only

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import { HomeCreateForm, ActionCard } from './ui-components'
+import { ActionCard } from './ui-components'
 
  
  
 function App() {
   return (
     <div className="App">
-      <HomeCreateForm />
       <ActionCard />
     </div>
   );
