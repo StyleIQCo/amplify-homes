0e0ace26cc36f3e7c8ee567a03666675ab97e8b9
StyleIQCo <95829027+StyleIQCo@users.noreply.github.com>
2024-01-24 20:49:36 -0800

    feature: ActionCard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import { HomeCreateForm } from './ui-components'
-
+import {
+  ActionCard 
+ } from './ui-components';
+ 
+ 
 function App() {
   return (
     <div className="App">
       <HomeCreateForm />
+      <ActionCard />
     </div>
   );
 }
